77b596f0954b808aebaef834334feac9e5b375ac
mariam-ass <mariamassaad010@gmail.com>
2023-12-20 14:26:22 +0200

    first commit

---
+++ 6218cc617f57927b6fb2.js
@@ -0,0 +1,19 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+// import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert("أضيف المُنتج إلى عربة الشراء")
+    })
+})
+
+
+
+
+
+document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear();
